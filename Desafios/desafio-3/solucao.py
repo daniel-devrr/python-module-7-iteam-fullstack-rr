e877ee75ff152314861b073877591e0cee5cde91
danielcassiano <danielcassianoxj6@gmail.com>
2026-05-24 11:30:01 -0300

    Corrigindo Desafio 3

--- Desafios/desafio-3/solucao.py
+++ Desafios/desafio-3/solucao.py
@@ -8,5 +8,5 @@ if vel_dig > 80 :
 elif vel_dig < 40 :
     print("Atenção! \nSua velocidade está abaixo do limite mínimo de 40km/h")
 
-elif vel_dig < 80 :
+elif vel_dig <= 80 and vel_dig >= 40:
     print("Boa viagem! \nDirija com segurança")
